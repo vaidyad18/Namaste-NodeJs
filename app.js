1907ef1d62e78ba8f5b2f46d62ee0d7770f4d0e3
Vaidya Dandriyal <vaidyadandriyal04@gmail.com>
2025-08-17 19:18:43 +0530

    create mongo database

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
-const mongodb = require("mongodb");
-const URI="mongodb+srv://namastenode:vaidya20080092@namastenode.ccvj8bv.mongodb.net/"
+const {MongoClient} = require("mongodb");
+const url="mongodb+srv://namastenode:vaidya20080092@namastenode.ccvj8bv.mongodb.net/";
+const client= new MongoClient(url);
+const dbName="HelloWorld";
+
+async function main() {
+  await client.connect();
+  //console.log('Connected successfully to server');
+
+  const db = client.db(dbName);
+  const collection = db.collection('User');
+
+  const data={
+        name:"Vishu",
+        city:"Mahindra Park",
+        phone:9999204808,
+        hobbies:["Gaming", "Coding", "Reading"],
+    }
+
+    // const insertResult = await collection.insertOne(data);
+    // console.log('Inserted documents =>', insertResult);
+
+    const findResult = await collection.find({name:"Vishu"}).count();
+    console.log('Found documents =>', findResult);
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
