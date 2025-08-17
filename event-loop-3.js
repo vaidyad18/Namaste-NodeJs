951d4891ce7669b8ba66bcdeff94169ec3314a5b
Vaidya Dandriyal <vaidyadandriyal04@gmail.com>
2025-08-17 21:49:03 +0530

    s1 done

---
+++ event-loop-3.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+setImmediate(() => console.log("setImmediate"));
+
+setTimeout(() => console.log("Timer expired"), 0);
+
+Promise.resolve("Promise").then(console.log);
+
+fs.readFile("./file.txt", "utf8", () => {
+  setTimeout(() => console.log("2nd timer"), 0);
+
+  process.nextTick(() => console.log("2nd nextTick"));
+
+  setImmediate(() => console.log(" 2nd setImmediate"));
+
+  console.log("File Reading CB");
+});
+process.nextTick(() => console.log("nextTick"));
+
+console.log("Last line of the file.");
